475e530dde5bff56231fa72ea213cfffcca77f5d
Devansh Rajput <devanshrajput032006@gmail.com>
2025-12-13 23:00:44 +0530

    update lab-page

---
+++ src/app/lab-page/page.jsx
@@ -0,0 +1,7 @@
+export default function LabPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-bold">Welcome to Lab Page</h1>
+    </div>
+  );
+}
